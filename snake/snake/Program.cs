2db5735fec1d6107ee999d8473059e1f472ce07b
Baksieuro <Baksieuro@gmail.com>
2019-03-20 02:02:32 +0500

    Инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +11,10 @@ namespace snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
             Console.ReadKey();
